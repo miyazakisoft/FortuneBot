86ec56249511c7b22fbbd822de1c35b52a9da89d
宮崎光 <kou@zakimiya-book.local>
2018-07-10 11:41:21 +0900

    Slackにおみくじの結果を投稿するプログラムを実装した。

--- src/main/kotlin/FortuneBot.kt
+++ src/main/kotlin/FortuneBot.kt
@@ -1,3 +1,6 @@
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+
 val random = java.util.Random()
 
 
@@ -14,9 +17,17 @@ open class RandomSelector<Element>(val rerultList : List<Element>){
 
 fun main(args: Array<String>){
     val randomSelector = RandomSelector(listOf<String>("大吉", "中吉", "吉", "小吉", "末吉", "凶", "大凶"))
+
+    /*
     (0..9)
             .map { i -> String.format("%d : %s", i, randomSelector.getResult()) }
             .forEach{ item -> println(item) }
+    */
+
+    val result = randomSelector.getResult()
+    val url = "https://slack.com/api/chat.postMessage?token=xoxp-367071642352-367245642241-376524142790-7080e417ddde55cac51d2377dc3cad42&channel=random&text=$result"
+
+    url.httpGet().response()
 
     return
 }
